d25043a150f64cb487a2046084fbe3596b8ba77c
Thomas O'Hara <thomas.ohara@adjusterpro.com>
2023-01-31 13:26:06 -0600

    xmlize.php: removed int from line 46

--- lib/xmlize.php
+++ lib/xmlize.php
@@ -43,7 +43,7 @@ class xml_format_exception extends moodle_exception {
     /** @var string */
     public $errorstring;
     /** @var int */
-    public int $line;
+    public $line;
     /** @var char */
     public $char;
     /**
